bdf8b85daa9fbd202bc9b79a2a00133c7de73204
naoki66 <naoki66@163.com>
2021-07-19 09:51:16 +0800

    Update bin.sh
    修改默认IP为192.168.11.1，修改主机名

--- bin.sh
+++ bin.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 # 修改默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 #修改主机名
-sed -i 's/OpenWrt/BinLean/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/AutoBuildLean/g' package/base-files/files/bin/config_generate
 #关闭自建私有源签名验证
 #sed -i '92d' package/system/opkg/Makefile
 
