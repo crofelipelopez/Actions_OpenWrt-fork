043927597af78fade2d1d68166f664f6dacdb9de
naoki66 <naoki66@163.com>
2022-03-01 11:22:56 +0800

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -12,7 +12,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 #添加额外软件包
 #git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
 #git clone  https://github.com/fw876/helloworld package/openwrt-packages/luci-app-helloworld
-#git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-packages/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-packages/luci-app-passwall
 git clone https://github.com/small-5/luci-app-adblock-plus package/openwrt-packages/luci-app-adblock-plus
 #git clone https://github.com/NagaseKouichi/openwrt-chinadns-ng.git package/chinadns-ng
 #git clone -b luci https://github.com/NagaseKouichi/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
@@ -37,8 +37,10 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/openwr
 #echo -e '#!/bin/sh\nbusybox poweroff' > ~/openwrt/files/sbin/shutdown
 #chmod 0755 ~/openwrt/files/sbin/shutdown
 
+#添加包含 passwall 的 feed 
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 
-#更新和安装库
+#更新和安装库 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
