81caa936b5557194dc3aac48de126ac7abd7a2e3
naoki66 <naoki66@163.com>
2022-03-30 23:58:04 +0800

    Update bin.sh
    升级smartdns版本20220329  1efa1942cc7afaa57d12625305469a45bca13f60

--- bin.sh
+++ bin.sh
@@ -50,8 +50,8 @@ echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.
 ./scripts/feeds install -a
 
 #升级smartdns版本
-sed -i 's/1.2021.35/2022.03.02/g' feeds/packages/net/smartdns/Makefile
-sed -i 's/f50e4dd0813da9300580f7188e44ed72a27ae79c/1fd18601e7d8ac88e8557682be7de3dc56e69105/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/1.2021.35/2022.03.29/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/f50e4dd0813da9300580f7188e44ed72a27ae79c/1efa1942cc7afaa57d12625305469a45bca13f60/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
 
 #升级zerotier版本
